51ba1671dd0debfb56cbdee36ba25a5eabc3d7d0
Oksana77777 <cassandra.bk@mail.ru>
2023-04-23 00:37:34 +0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,27 +10,24 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
     public static void main(String[] args) {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
-                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
-                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
                         .build())
                 .build();
 
         try (CloseableHttpResponse response = httpClient.execute(new HttpGet(URL))) {
-          //  Scanner sc = new Scanner(response.getEntity().getContent());
-          //  while (sc.hasNext()) {
-          //      System.out.println(sc.nextLine());
-          //  }
             ObjectMapper mapper = new ObjectMapper();
             List<CatFacts> facts = mapper.readValue(response.getEntity().getContent(),
-                    new TypeReference<List<CatFacts>>() {});
+                    new TypeReference<List<CatFacts>>() {
+                    });
             facts.stream()
                     .filter(fact -> fact.getUpvotes() > 0)
                     .forEach(System.out::println);
@@ -39,9 +35,5 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
-        //  HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
-      //  CloseableHttpResponse response = httpClient.execute(request);
     }
 }
